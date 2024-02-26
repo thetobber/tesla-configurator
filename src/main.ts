803d4c301bc4c925a27a84dae4a383e78797c6e4
thetobber <cchawps@gmail.com>
2024-02-26 16:23:35 +0100

    updated packages and added tailwind

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { setupWorker } from 'msw/browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
-);
+bootstrapApplication(AppComponent, appConfig).catch(console.error);
 
 const handlers = [
   http.get('/options/:id', ({ params }) => {
